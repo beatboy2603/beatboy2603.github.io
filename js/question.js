d3c6b4acd710e3517794b9068fbe68bbfae86c0c
Quang Huy Le <beatboy2603@gmail.com>
2018-11-22 01:15:49 +0700

    123

--- js/question.js
+++ js/question.js
@@ -38,6 +38,6 @@ var x = setInterval(function () {
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        $('#div-countdown p').text("Expired");
     }
 }, 1000);
