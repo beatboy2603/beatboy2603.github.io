d00e7e9f24dea35338ea8124c40ef13997f618b1
Quang Huy Le <beatboy2603@gmail.com>
2018-11-22 01:44:04 +0700

    123

--- js/question.js
+++ js/question.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         url: 'https://beatboy2603.github.io/html/question1.html',
         method: 'GET',
         success: function (data) {
-            $('#demo').text(data.question);
+            $('#demo').text(data);
             console.log("success!", data);
         },
         error: function () {
