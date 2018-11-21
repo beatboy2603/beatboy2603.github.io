be7e2132591f760802c37d8e161ac28ee390609a
Quang Huy Le <beatboy2603@gmail.com>
2018-11-22 02:11:03 +0700

    123

--- js/question.js
+++ js/question.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         url: 'https://beatboy2603.herokuapp.com/html/question1.html',
         method: 'GET',
         success: function (data) {
-            $('#demo').text(data);
+            $('#demo').text(data.question);
             console.log("success!", data);
         },
         error: function () {
